d7f2117c3a628fee476cf6aadc018bb7e3031015
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-04-08 16:05:01 +0200

    ADD service

--- src/app/gifs/resultados/resultados.component.ts
+++ src/app/gifs/resultados/resultados.component.ts
@@ -1,16 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-resultados',
   templateUrl: './resultados.component.html',
-  styles: [
-  ]
 })
-export class ResultadosComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class ResultadosComponent {}
